f739d537c2e7880aa871e4b73b914eb8420996bb
max <maksim@cs.fsu.edu>
2021-07-29 09:19:28 -0400

    get rid of unnecessary intermediate

--- bank.py
+++ bank.py
@@ -1,10 +1,10 @@
 from .parse import parse, AMRIO
+from tqdm import tqdm
 
 def parse_amr_bank(filename: str, **parse_kwargs):
-    amrs_in = list(AMRIO.read(filename))
 
-    sentences = [amr.sentence for amr in amrs_in]
+    sentences = [amr.sentence \
+        for amr in \
+            tqdm(AMRIO.read(filename), desc=f"Reading from {filename}")]
 
     return parse(sentences, **parse_kwargs)
-
-
